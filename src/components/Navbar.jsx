e8f32d941309a1bb6220e5606a1b50679cf5b74b
Vineet Jadhav <vineet290403@gmail.com>
2024-11-26 23:49:53 +0530

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,48 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button'
 import { TiLocationArrow } from 'react-icons/ti'
+import { useWindowScroll } from 'react-use';
+import gsap from "gsap";
 
 const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
 
 const Navbar = () => {
 
+    const { y: currentScrollY } = useWindowScroll()
+
     const [isAudioPlaying, setisAudioPlaying] = useState(false);
     const [isIndicatorActive, setisIndicatorActive] = useState(false)
+    const [lastScrollY, setlastScrollY] = useState(0)
+    const [isNavVisible, setisNavVisible] = useState(true)
+
+
+    useEffect(() => {
+        if (currentScrollY === 0) {
+            setisNavVisible(true)
+            navContainerRef.current.classList.remove('floating-nav')
+        }
+
+        //*Scrolling from top to bottom
+        else if (currentScrollY > lastScrollY) {
+            setisNavVisible(false)
+            navContainerRef.current.classList.add('floating-nav')
+        }
+        //*Scrolling from bottom to top
+        else if (currentScrollY < lastScrollY) {
+            setisNavVisible(true)
+            navContainerRef.current.classList.add('floating-nav')
+
+        }
+        setlastScrollY(currentScrollY)
+    }, [currentScrollY])
+
+    useEffect(() => {
+        gsap.to(navContainerRef.current, {
+            y: isNavVisible ? 0 : -100,
+            opacity: isNavVisible ? 1 : 0,
+            duration: 0.2,
+        });
+    }, [isNavVisible]);
 
     const navContainerRef = useRef(null)
     const audioRef = useRef(null)
@@ -25,6 +60,8 @@ const Navbar = () => {
             audioRef.current.pause()
         }
     }, [isAudioPlaying])
+
+
 
     return (
         <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-0'>
